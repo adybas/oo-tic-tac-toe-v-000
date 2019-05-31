f00c919521db33180d1add78ecb0d881218ce004
Anna Dybas <a.m.dybas@gmail.com>
2019-05-31 18:00:32 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -33,8 +33,8 @@ class TicTacToe
     @board[index] != " " ? true : false
   end
 
-  def valid_move?(board, index)
-    index.between?(0,8) && !position_taken?(board, index)
+  def valid_move?(index)
+    index.between?(0,8) && !position_taken?(index)
   end
 
   def turn(board)
